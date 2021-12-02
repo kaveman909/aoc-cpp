1f0b12b2f9a4f256f73a20ac15b261b37b85543a
David Tracy <david.tracy3@gmail.com>
2021-12-01 17:09:12 -0800

    Day 1 Pt 2

--- src/days/1.cpp
+++ src/days/1.cpp
@@ -8,14 +8,31 @@
 #include "fmt/core.h"
 #include "fmt/ranges.h"
 
+auto diff_and_count(const std::vector<int> &in) {
+  std::vector<int> diffs;
+  std::adjacent_difference(in.begin(), in.end(), std::back_inserter(diffs));
+  const auto out = std::count_if(diffs.begin() + 1, diffs.end(),
+                                 [](int e) { return e > 0; });
+  return out;
+}
+
 int main(int argc, char *argv[]) {
   assert(argc == 2);
 
+  // Part 1
   const auto depths =
       process_input(argv[1], [](const std::string &x) { return std::stoi(x); });
-  auto diffs = depths;
-  std::adjacent_difference(diffs.begin(), diffs.end(), diffs.begin());
-  const auto out = std::count_if(diffs.begin() + 1, diffs.end(),
-                                 [](int e) { return e > 0; });
-  fmt::print("Part 1: {}\n", out);
+  fmt::print("Part 1: {}\n", diff_and_count(depths));
+
+  // Part 2
+  std::vector<int> sums;
+  // Add first and second
+  std::transform(depths.begin(), depths.end() - 1, depths.begin() + 1,
+                 std::back_inserter(sums), std::plus<int>());
+  // now add the third to the existing sum
+  std::transform(sums.begin(), sums.end() - 1, depths.begin() + 2, sums.begin(),
+                 std::plus<int>());
+  // (one extra element will exist at the end of 'sums' at this point)
+  sums.erase(sums.end() - 1);
+  fmt::print("Part 2: {}\n", diff_and_count(sums));
 }
