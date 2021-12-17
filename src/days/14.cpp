1368b80e8adaacf80ad3b97596012ab17c8a06bc
David Tracy <david.tracy3@gmail.com>
2021-12-16 18:09:42 -0800

    Day 14 Pt 2

--- src/days/14.cpp
+++ src/days/14.cpp
@@ -1,10 +1,14 @@
 #include "aoc_includes.h"
 
 auto local_process_input(char *fpath) {
-  std::unordered_map<std::string, std::string> pair_rules;
+  std::unordered_map<std::string, std::pair<std::string, std::string>>
+      pair_rules;
 
   std::ifstream in(fpath);
   char buf[MAX_LEN];
+  std::unordered_map<std::string, long> pair_counters;
+  std::unordered_map<char, long> single_counters;
+
   in.getline(buf, MAX_LEN);  // first line is polymer template
   std::string poly_template{buf};
   in.getline(buf, MAX_LEN);  // empty line
@@ -14,37 +18,54 @@ auto local_process_input(char *fpath) {
     std::vector<std::string> parsed;
     boost::split(parsed, s, boost::is_any_of(" ->"),
                  boost::algorithm::token_compress_on);
-    pair_rules[parsed[0]] = parsed[1];
+    pair_rules[parsed[0]].first = parsed[0][0] + parsed[1];
+    pair_rules[parsed[0]].second = parsed[1] + parsed[0][1];
+    pair_counters[parsed[0]] = 0;
+    single_counters[parsed[1][0]] = 0;
   }
-  return std::pair{poly_template, pair_rules};
+  return std::tuple{poly_template, pair_rules, pair_counters, single_counters};
 }
 
 void aoc(char *f) {
-  auto [polymer, pair_rules] = local_process_input(f);
+  auto [polymer, pair_rules, pair_counters, single_counters] =
+      local_process_input(f);
 
-  for (int i = 0; i < 10; i++) {
-    // Calculate new inserts
-    std::string new_inserts;
-    for (size_t i = 0; i < polymer.length() - 1; i++) {
-      new_inserts += pair_rules[polymer.substr(i, 2)];
-    }
-    // Create new string with interleaved data
-    std::string combined;
-    for (auto &&[p, n] : iter::zip_longest(polymer, new_inserts)) {
-      if (p) combined += *p;
-      if (n) combined += *n;
-    }
-    polymer = combined;
+  // (1) Get initial counts
+  for (size_t i = 0; i < polymer.length() - 1; i++) {
+    pair_counters[polymer.substr(i, 2)]++;
   }
 
-  std::sort(polymer.begin(), polymer.end());
-  std::string poly_unique;
-  std::unique_copy(polymer.begin(), polymer.end(),
-                   std::back_inserter(poly_unique));
-  std::vector<int> poly_counts;
-  for (const auto &el : poly_unique) {
-    poly_counts.emplace_back(std::count(polymer.begin(), polymer.end(), el));
+  // (2) Run the Simulation
+  {
+    MeasureTime m{"Sim Time"};
+    for (int i = 0; i < 40; i++) {
+      auto pair_counters_cp = pair_counters;
+      for (const auto &rule : pair_rules) {
+        if (pair_counters_cp[rule.first] > 0) {
+          long count = pair_counters_cp[rule.first];
+          const auto &[new_a, new_b] = rule.second;
+          pair_counters[rule.first] -= count;
+          pair_counters[new_a] += count;
+          pair_counters[new_b] += count;
+        }
+      }
+    }
   }
-  std::sort(poly_counts.begin(), poly_counts.end());
-  fmt::print("Part 1: {}\n", *(poly_counts.end() - 1) - *poly_counts.begin());
+
+  // (3) Get the single poly counts
+  for (auto &count : pair_counters) {
+    for (int i = 0; i < 2; i++) {
+      single_counters[count.first[i]] += count.second;
+    }
+  }
+  std::for_each(single_counters.begin(), single_counters.end(),
+                [](auto &count) { count.second /= 2; });
+  // compensate for first/last element in polymer not being double-counted
+  single_counters[polymer.front()]++;
+  single_counters[polymer.back()]++;
+
+  auto [min, max] = std::minmax_element(
+      single_counters.begin(), single_counters.end(),
+      [](const auto &p1, const auto &p2) { return p1.second < p2.second; });
+  fmt::print("Part 2: {}\n", max->second - min->second);
 }
