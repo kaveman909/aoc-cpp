b88ca0cc30cba86aa3c0c914c9cbe36d6a913a3d
David Tracy <david.tracy3@gmail.com>
2022-12-03 14:22:42 -0800

    added MeasureTime calls to day 3

--- src/2022/3.cpp
+++ src/2022/3.cpp
@@ -7,8 +7,8 @@ int priority(char c) {
   return c - 'A' + 27;
 }
 
-std::string rucksack_compare(const std::string &ruck_a,
-                             const std::string &ruck_b) {
+std::string rucksack_compare(const std::string_view ruck_a,
+                             const std::string_view ruck_b) {
   std::unordered_set<char> ruck_a_set;
   std::unordered_set<char> overlap_set;
   std::string overlap;
@@ -29,29 +29,27 @@ std::string rucksack_compare(const std::string &ruck_a,
 void aoc(char *f) {
   const auto rucksacks = process_input(f);
   int sum = 0;
-  for (const auto &rucksack : rucksacks) {
-    std::unordered_set<char> left_comp;
-    const auto &half = rucksack.size() / 2;
-    std::string_view left(rucksack.c_str(), half);
-    std::string_view right(rucksack.c_str() + half, half);
-    for (const auto item : left) {
-      left_comp.insert(item);
-    }
-    for (const auto item : right) {
-      if (left_comp.contains(item)) {
-        sum += priority(item);
-        break;
-      }
+  {
+    MeasureTime m("Part 1");
+
+    for (const auto &rucksack : rucksacks) {
+      const auto &half = rucksack.size() / 2;
+      const std::string_view left(rucksack.c_str(), half);
+      const std::string_view right(rucksack.c_str() + half, half);
+      sum += priority(rucksack_compare(left, right)[0]);
     }
   }
   fmt::print("Part 1: {}\n", sum);
 
   int sum2 = 0;
-  assert(rucksacks.size() % 3 == 0);
-  for (size_t i = 0; i < rucksacks.size(); i += 3) {
-    const auto comp1 = rucksack_compare(rucksacks[i], rucksacks[i + 1]);
-    const auto comp2 = rucksack_compare(comp1, rucksacks[i + 2]);
-    sum2 += priority(comp2[0]);
+  {
+    MeasureTime m("Part 2");
+    assert(rucksacks.size() % 3 == 0);
+    for (size_t i = 0; i < rucksacks.size(); i += 3) {
+      const auto comp1 = rucksack_compare(rucksacks[i], rucksacks[i + 1]);
+      const auto comp2 = rucksack_compare(comp1, rucksacks[i + 2]);
+      sum2 += priority(comp2[0]);
+    }
   }
   fmt::print("Part 2: {}\n", sum2);
 }
