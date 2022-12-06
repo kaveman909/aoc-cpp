041f076c8e75d133c445ed1b10e3d54a2c211d52
David Tracy <david.tracy3@gmail.com>
2022-12-05 23:06:00 -0800

    Added timings

--- src/2022/6.cpp
+++ src/2022/6.cpp
@@ -1,14 +1,22 @@
 #include "aoc_includes.h"
+using namespace std::string_literals;
 
-size_t get_marker(const std::string &signal, size_t sz) {
-  for (size_t i = 0; i < signal.size() - sz; i++) {
-    std::string_view header(signal.begin() + i, signal.begin() + i + sz);
-    std::unordered_set<char> set;
-    for (const char c : header) {
-      set.emplace(c);
-    }
-    if (set.size() == sz) {
-      return i + sz;
+size_t get_marker(const std::string &signal, const size_t sz) {
+  {
+    MeasureTime m{"Marker Size = "s + std::to_string(sz)};
+    for (size_t i = 0; i < signal.size() - sz; i++) {
+      const std::string_view header(signal.begin() + i, signal.begin() + i + sz);
+      std::unordered_set<char> set;
+      bool valid = true;
+      for (const char c : header) {
+        if(!set.emplace(c).second) {
+          valid = false;
+          break;
+        }
+      }
+      if (valid) {
+        return i + sz;
+      }
     }
   }
   return 0;
