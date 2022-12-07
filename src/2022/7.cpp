7fb06072b8e988f82c4b817fd9700c039b8f37ae
David Tracy <david.tracy3@gmail.com>
2022-12-07 13:58:47 -0800

    day 7 part 2

--- src/2022/7.cpp
+++ src/2022/7.cpp
@@ -7,8 +7,10 @@ class Dir {
  public:
   std::string name;
   Dir *parent;
+  size_t this_dir_size = 0;
   size_t total_size = 0;
   static size_t global_sum;
+  static size_t delete_target;
 
   Dir(const std::string &_name, Dir *_parent) : name(_name), parent(_parent) {}
 
@@ -27,26 +29,30 @@ class Dir {
     return &children.back();
   }
 
-  // Prints info via DFS
-  void print_tree(const int level) const {
-    fmt::print("{}\t{}\t{}\n", name, total_size, level);
+  size_t sum_files() {
+    total_size = this_dir_size;
     for (auto &child : children) {
-      child.print_tree(level + 1);
+      total_size += child.sum_files();
     }
+    if (total_size <= 100'000) {
+      global_sum += total_size;
+    }
+    return total_size;
   }
 
-  size_t sum_files(void) {
-    size_t local_sum = total_size;
+  size_t find_delete_file(size_t current_min) {
+    if (total_size >= delete_target) {
+      // viable candiate for deletion
+      current_min = std::min(current_min, total_size);
+    }
     for (auto &child : children) {
-      local_sum += child.sum_files();
+      current_min = std::min(current_min, child.find_delete_file(current_min));
     }
-    if (local_sum <= 100'000) {
-      global_sum += local_sum;
-    }
-    return local_sum;
+    return current_min;
   }
 };
 size_t Dir::global_sum;
+size_t Dir::delete_target;
 
 void aoc(char *f) {
   const auto cmds = process_input(f);
@@ -81,10 +87,17 @@ void aoc(char *f) {
                    scn::scan_tuple<size_t, std::string>(cmd, "{} {}");
                result) {
       // (ignore the file_name for now, doesn't apply to pt 1)
-      active_dir->total_size += file_size;
+      active_dir->this_dir_size += file_size;
     }
   }
 
-  root.sum_files();
+  const auto top_level_size = root.sum_files();
   fmt::print("Part 1: {}\n", Dir::global_sum);
+
+  const auto free_space_current = 70'000'000 - top_level_size;
+  const auto delete_target = 30'000'000 - free_space_current;
+  Dir::delete_target = delete_target;
+  const auto size_delete_file =
+      root.find_delete_file(std::numeric_limits<size_t>::max());
+  fmt::print("Part 2: {}\n", size_delete_file);
 }
